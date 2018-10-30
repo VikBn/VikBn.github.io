76ba4c167bd045e6be51585ebae5e009eb3dcb02
VikBn <32108055+VikBn@users.noreply.github.com>
2018-10-30 22:31:53 +0200

    Add files via upload

---
+++ app/js/index.js
@@ -0,0 +1,19 @@
+// var label = $('.sidebar__confirm').find('label');
+//
+// label.on('click', function(){
+//   var input = $(this).parent().find('.ico-checkbox');
+//   console.log(input);
+//   input.toggleClass('sed');
+// });
+//
+// $(document).ready(function() {
+//   $('select').niceSelect();
+// });
+//
+// $('input').checks({
+//   class_radio: 'ico-radio',
+//   class_checkbox: 'ico-checkbox',
+//   class_checked: 'sed',
+//   class_disabled: 'disabled',
+//   types: ['radio', 'checkbox']
+// });
